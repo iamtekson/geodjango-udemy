2acf8f4cea8ecda1d134816783428143ddf634fe
iamtekson <teksondada@gmail.com>
2020-08-06 10:36:56 +0700

    geoserver implementation

--- gic/settings.py
+++ gic/settings.py
@@ -125,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'gic/static')]
